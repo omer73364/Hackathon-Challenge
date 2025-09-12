e466f5852af9167feef6093a16336d03256627c7
Omar Anwar <omar.anwar@transforat.com>
2025-09-12 22:07:43 +0300

    create table if not exists in db

--- db.js
+++ db.js
@@ -12,4 +12,20 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
+pool.execute(`
+    CREATE TABLE IF NOT EXISTS fingerprints (
+      id int(11) NOT NULL AUTO_INCREMENT,
+      name varchar(255) DEFAULT NULL,
+      fingerprint_img varchar(255) NOT NULL,
+      matched tinyint(1) NOT NULL,
+      score int(11) NOT NULL,
+      created_at timestamp NOT NULL DEFAULT current_timestamp(),
+      PRIMARY KEY (id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+`).then(() => {
+  console.log("Table `fingerprints` created if not exist");
+}).catch((error) => {
+  console.error("Error creating table `fingerprints`: ", error);
+});
+
 module.exports = pool;
