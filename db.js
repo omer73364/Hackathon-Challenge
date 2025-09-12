a5de07788b43561cd3f8a075dca58686eefe97f6
Omar Anwar <omar.anwar@transforat.com>
2025-09-12 21:57:34 +0300

    database connection

---
+++ db.js
@@ -0,0 +1,15 @@
+// db.js
+const mysql = require("mysql2/promise"); // Using the promise-based API
+require("dotenv").config(); // For environment variables
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
