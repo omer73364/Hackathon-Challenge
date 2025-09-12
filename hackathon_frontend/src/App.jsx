b70cd2ee63e1117689d61a23319abaf9505b6c80
DEKU9873 <alhassan9873@gmail.com>
2025-09-12 10:57:51 +0300

    setup project

---
+++ hackathon_frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div className='bg-red-500'>
+           <Button>Click me</Button>
+
+    </div>
+  )
+}
+
+export default App
